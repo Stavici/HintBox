c5cf6a6a0c8a79af73fa6d4fad61c51585d7c54d
root <stanislav.kovnacky@cosmos2.ru>
2025-07-14 12:21:47 +0300

    refactor(cube): remove custom comparator, use IntBinaryOperator, add clear Javadoc and consistent naming

--- app/src/main/java/com/example/hintbox/Cuber.java
+++ app/src/main/java/com/example/hintbox/Cuber.java
@@ -1,71 +1,132 @@
 package com.example.hintbox;
 
-public class Cuber {
-    public int x, y, w, h;
-    public String color;
+import java.util.function.IntBinaryOperator;
 
-    Cuber (int x_, int y_, int w_, int h_, String c) {
-        this.x = x_;
-        this.y = y_;
-        this.w = w_;
-        this.h = h_;
-        this.color = c;
+/**
+ * Интерфейс для перемещения объекта по координатам.
+ */
+interface Movable {
+    int getX();
+    int getY();
+    void setX(int x);
+    void setY(int y);
+}
+
+/**
+ * Интерфейс для изменения размеров объекта.
+ */
+interface Resizable {
+    int getWidth();
+    int getHeight();
+    void setWidth(int width);
+    void setHeight(int height);
+}
+
+/**
+ * Интерфейс для управления цветом объекта.
+ */
+interface Colorable {
+    String getColor();
+    void setColor(String color);
+}
+
+/**
+ * Класс модели куба с возможностью движения, изменения размера и цвета.
+ */
+public class Cube implements Movable, Resizable, Colorable {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private String color;
+
+    /**
+     * @param x начальная координата X
+     * @param y начальная координата Y
+     * @param width ширина куба
+     * @param height высота куба
+     * @param color цвет куба (имя или код)
+     */
+    public Cube(int x, int y, int width, int height, String color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
     }
 
-    public int getY() {
-        return y;
-    }
-
+    // Movable
+    @Override
     public int getX() {
         return x;
     }
 
-    public String getColor() {
-        return color;
+    @Override
+    public int getY() {
+        return y;
     }
 
-    public int getH() {
-        return h;
-    }
-
-    public int getW() {
-        return w;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
+    @Override
     public void setX(int x) {
         this.x = x;
     }
 
+    @Override
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    // Resizable
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    @Override
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    // Colorable
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
     public void setColor(String color) {
         this.color = color;
     }
 
-    public void setH(int h) {
-        this.h = h;
+    /**
+     * Возвращает максимальное значение по X между текущей координатой и переданным значением.
+     *
+     * @param otherX значение для сравнения
+     * @param comparator функция сравнения двух целых чисел
+     * @return результат сравнения
+     */
+    public int maxX(int otherX, IntBinaryOperator comparator) {
+        return comparator.applyAsInt(this.x, otherX);
     }
 
-    public void setW(int w) {
-        this.w = w;
+    /**
+     * Возвращает максимальное значение по Y между текущей координатой и переданным значением.
+     *
+     * @param otherY значение для сравнения
+     * @param comparator функция сравнения двух целых чисел
+     * @return результат сравнения
+     */
+    public int maxY(int otherY, IntBinaryOperator comparator) {
+        return comparator.applyAsInt(this.y, otherY);
     }
-
-    public int compareToX(int xC) {
-        if (xC > this.x) {
-            return xC;
-        } else {
-            return this.x;
-        }
-    }
-
-    public int compareToY(int yC) {
-        if (yC > this.y) {
-            return yC;
-        } else {
-            return this.y;
-        }
-    }
-
 }
